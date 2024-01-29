b27f16bbc7d714cc91091ae77340b53f28614f48
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-01-29 14:18:56 +0700

    CNTL_FRAMEWORK.py

---
+++ Data-Engineering/Airflow/CNTL_FRAMEWORK.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.models.param import Param
+from airflow.utils.dates import days_ago
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+dag = DAG(
+    'CNTL_FRAMEWORK',
+    default_args=default_args,
+    description='Running Strem',
+    schedule_interval=None,
+    tags=['e2e example','ETL', 'spark'],
+    params={
+        'STREM_NM': Param("X3_TEST_99_D", type="string"),
+    },
+    access_control={
+        'All': {
+            'can_read',
+            'can_edit',
+            'can_delete'
+        }
+    }
+)
+
+def start_job():
+    print("Start ETL by Query...")
+
+def end_job():
+    print("Data insert to Table Done...")
+
+task1 = PythonOperator(
+    task_id='Start_Data_Reading',
+    python_callable=start_job,
+    dag=dag,
+)
+task2=SparkKubernetesOperator(
+    task_id='Spark_etl_submit',
+    application_file="CNTL_FRAMEWORK.yaml",
+    do_xcom_push=True,
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    enable_impersonation_from_ldap_user=True
+)
+
+task3 = SparkKubernetesSensor(
+    task_id='Spark_etl_monitor',
+    application_name="{{ task_instance.xcom_pull(task_ids='Spark_etl_submit')['metadata']['name'] }}",
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    attach_log=True
+)
+
+task4 = PythonOperator(
+    task_id='Data_Loading_Done',
+    python_callable=end_job,
+    dag=dag,
+)
+
+task1>>task2>>task3>>task4
