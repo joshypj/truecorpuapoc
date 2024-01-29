c5b8f2dba685505d04d1a8351bde8ed2dc77fa0a
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-01-29 10:32:37 +0700

    test_bash.py

--- Data-Engineering/Airflow/test_bash.py
+++ Data-Engineering/Airflow/test_bash.py
@@ -4,11 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
-# Define a Python function to print XCom data
-def print_xcom_data(**kwargs):
-    ti = kwargs['ti']
-    ls_output = ti.xcom_pull(task_ids='run_ls_command')
-    print("Output of 'ls' command:", ls_output)
+
 
 default_args = {
     'owner': 'airflow',
@@ -33,10 +29,16 @@ dag = DAG(
     }
 )
 
+# Define a Python function to print XCom data
+def print_xcom_data(**kwargs):
+    ti = kwargs['ti']
+    ls_output = ti.xcom_pull(task_ids='run_ls_command')
+    print("Output of 'ls' command:", ls_output)
+
 # Define the BashOperator to run the ls command
 run_ls_command = BashOperator(
     task_id='run_ls_command',
-    bash_command='ls',
+    bash_command='ls /mnt',
     dag=dag,
 )
 
