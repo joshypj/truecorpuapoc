0f9c5f3eb7ff5d6d49901004359bf948b8c8fa85
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-01-30 13:32:41 +0700

    CNTL_FRAMEWORK.py

--- Data-Engineering/Airflow/CNTL_FRAMEWORK.py
+++ Data-Engineering/Airflow/CNTL_FRAMEWORK.py
@@ -74,7 +74,7 @@ task2 = SparkKubernetesOperator(
     dag=dag,
     api_group="sparkoperator.hpe.com",
     enable_impersonation_from_ldap_user=True,
-    params = _get_arguments, # Pass the function directly to arguments
+    params = {"strem_nm": "TEST"}, # Pass the function directly to arguments
 )
 
 task3 = SparkKubernetesSensor(
