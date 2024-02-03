11e575e79ede8d65a82828d3a866e8a72cd5d5a1
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-02-03 14:26:38 +0700

    CNTL_FRAMEWORK.py

--- Data-Engineering/Airflow/CNTL_FRAMEWORK.py
+++ Data-Engineering/Airflow/CNTL_FRAMEWORK.py
@@ -65,7 +65,7 @@ def submit_spark_etl(**kwargs):
         api_group="sparkoperator.hpe.com",
         enable_impersonation_from_ldap_user=True,
         dag=dag,
-        parameters= param
+        params= param
     )
     return 'Spark_etl_submit'
 
