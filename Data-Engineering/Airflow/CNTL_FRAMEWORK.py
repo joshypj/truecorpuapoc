308245465ddfe0b202c356db73adac99f73f772f
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-02-03 14:05:06 +0700

    CNTL_FRAMEWORK.py

--- Data-Engineering/Airflow/CNTL_FRAMEWORK.py
+++ Data-Engineering/Airflow/CNTL_FRAMEWORK.py
@@ -57,6 +57,13 @@ def condition(**kwargs):
 
 def submit_spark_etl(**kwargs):
     parameter_value = kwargs['ti'].xcom_pull(task_ids='read_file', key='file_content')
+    parameters = {}  # Initialize an empty dictionary
+    try:
+        # Split the parameter value and convert it to a dictionary
+        parameters = dict(item.split('=') for item in parameter_value.split('^|')[1].split(';'))
+    except ValueError as e:
+        print(f"Error converting parameter value to dictionary: {e}")
+
     spark_operator = SparkKubernetesOperator(
         task_id='Spark_etl_submit',
         application_file="test_cntl.yaml",
@@ -64,7 +71,7 @@ def submit_spark_etl(**kwargs):
         api_group="sparkoperator.hpe.com",
         enable_impersonation_from_ldap_user=True,
         dag=dag,
-        parameters= parameter_value.split('^|')[1]
+        parameters=parameters  # Pass the parameters dictionary
     )
     return 'Spark_etl_submit'
 
