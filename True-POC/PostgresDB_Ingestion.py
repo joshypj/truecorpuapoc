a342ac5a2d0dbf30dc6bf0785587e6ad3c6f5174
ezmeralpoc <ezmeralpoc@gmail.com>
2024-02-04 21:11:23 +0700

    done

--- True-POC/PostgresDB_Ingestion.py
+++ True-POC/PostgresDB_Ingestion.py
@@ -14,7 +14,7 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 dag = DAG(
-    'OracleDB_Ingestion',
+    'PostgresDB_Ingestion',
     default_args=default_args,
     description='Banking-data-demo',
     schedule_interval=None,
