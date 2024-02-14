335cde13e266868791c1b7cff26e75746689ad33
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-02-15 01:11:28 +0700

    CNTL_FRAMEWORK.py

--- Data-Engineering/Airflow/CNTL_FRAMEWORK.py
+++ Data-Engineering/Airflow/CNTL_FRAMEWORK.py
@@ -103,8 +103,7 @@ def processing(**kwargs):
     df = pd.read_csv(file_path)
     for index, row in df.iterrows() :
         params = row['parm']
-        print(row['prcs_typ'],type(row['prcs_typ']))
-        if row['prcs_typ'] == '1' :
+        if row['prcs_typ'] == 1 :
             source_path = params.split('^|')[0]
             dest_path = params.split('^|')[1]
             print(source_path,dest_path)
