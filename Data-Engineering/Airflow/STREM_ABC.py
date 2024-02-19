53d4d0f855c4f3c3a47b7948695cb9193a28ad6d
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-02-19 13:45:58 +0700

    STREM_ABC.py

---
+++ Data-Engineering/Airflow/STREM_ABC.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.models import Param,DagRun
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+
+# Define default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+# Define the DAG
+dag = DAG(
+    'STREM_ABC',
+    default_args=default_args,
+    description='Running Stream',
+    schedule_interval=None,
+    tags=['e2e example', 'ETL', 'spark'],
+    params={'STREM_NM': Param("X3_TEST_99_D", type="string")},
+    access_control={'All': {'can_read', 'can_edit', 'can_delete'}}
+)
+
+
+def get_strem_nm(**kwargs):
+    strem_nm = kwargs["params"]["STREM_NM"]
+    print(strem_nm)
+
+START_STREM = SparkKubernetesOperator(
+    task_id='Spark_etl_submit',
+    application_file="START_STREM.yaml",
+    do_xcom_push=True,
+    api_group="sparkoperator.hpe.com",
+    enable_impersonation_from_ldap_user=True,
+    dag=dag,
+)
+
+START_STREM
