5d3ba83350a0949923f571a807c73e7972cff7fa
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-02-19 23:07:09 +0700

    GENERATE_DAG.py

--- Data-Engineering/Airflow/GENERATE_DAG.py
+++ Data-Engineering/Airflow/GENERATE_DAG.py
@@ -1,38 +1,20 @@
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
+from airflow.decorators import dag, task
 
-# Define a function to generate DAG dynamically
-def generate_dag(dag_id, schedule, default_args):
-    dag = DAG(
-        dag_id=dag_id,
-        default_args=default_args,
-        schedule_interval=schedule,
-        catchup=False
-    )
-
-    # Define tasks dynamically based on some criteria
-    with dag:
-        start = DummyOperator(task_id='start')
-        end = DummyOperator(task_id='end')
-
-        # Define additional tasks based on your requirements
-        # You can use loops or conditionals here to define tasks dynamically
-
-        start >> end
-
-    return dag
-
-# Define default arguments for DAGs
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2024, 2, 19),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1
+configs = {
+    "config1": {"message": "first DAG will receive this message"},
+    "config2": {"message": "second DAG will receive this message"},
 }
 
-# Example of dynamically generating a DAG
-generated_dag_id = 'dynamic_dag_example'
-generated_schedule = '0 0 * * *'  # Run daily at midnight
-dynamic_dag = generate_dag(generated_dag_id, generated_schedule, default_args)
+for config_name, config in configs.items():
+    dag_id = f"dynamic_generated_dag_{config_name}"
+
+    @dag(dag_id=dag_id, start_date=datetime(2022, 2, 1))
+    def dynamic_generated_dag():
+        @task
+        def print_message(message):
+            print(message)
+
+        print_message(config["message"])
+
+    dynamic_generated_dag()
