071e36ace929fde0c1bc8a5fc63504b93929c8c4
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-02-19 20:25:01 +0700

    CHILD.py

--- Data-Engineering/Airflow/CHILD_DAG.py
+++ Data-Engineering/Airflow/CHILD_DAG.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.models import Param
+from airflow.utils.dates import days_ago
+from airflow.models import Param,DagRun
 from airflow.operators.python_operator import PythonOperator
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.utils.dates import days_ago
-
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 # Define default arguments
 default_args = {
     'owner': 'airflow',
@@ -25,13 +25,11 @@ dag = DAG(
     access_control={'All': {'can_read', 'can_edit', 'can_delete'}}
 )
 
+
 def get_strem_nm(**kwargs):
     strem_nm = kwargs["params"]["STREM_NM"]
     return strem_nm
 
-def pass_strem_nm(**kwargs):
-    strem_nm = kwargs['task_instance'].xcom_pull(task_ids='get_strem_nm')
-    return strem_nm
 
 task1 = PythonOperator(
     task_id='get_strem_nm',
@@ -40,22 +38,23 @@ task1 = PythonOperator(
     dag=dag,
 )
 
-task2 = PythonOperator(
-    task_id='pass_strem_nm',
-    python_callable=pass_strem_nm,
-    provide_context=True,
-    dag=dag,
-)
+def pass_strem_nm(**kwargs):
+    return kwargs['ti'].xcom_pull(task_ids='get_strem_nm')
 
 spark_operator_task = SparkKubernetesOperator(
     task_id='Spark_etl_submit',
     application_file="test_cntl.yaml",
     do_xcom_push=True,
-    arguments=[task2.output],
+    arguments=["{{ params.strem_nm }}"],
     dag=dag,
     pi_group="sparkoperator.hpe.com",
     enable_impersonation_from_ldap_user=True
 )
 
-# Define task dependencies
-task1 >> task2 >> spark_operator_task
+# Set the parameter you want to pass to the Spark application
+spark_operator_task.params = {
+    'strem_nm': 'TEST'
+}
+
+
+task1 >> spark_operator_task
