5c39e8a8ff328b03bbd7928dee523623a016e65e
NOS12110343\thana <Thanakit.suv@inteltion.com>
2024-02-20 21:07:20 +0700

    GENERATE_DAG.py

--- Data-Engineering/Airflow/GENERATE_DAG.py
+++ Data-Engineering/Airflow/GENERATE_DAG.py
@@ -7,8 +7,13 @@ from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKube
 import pandas as pd
 
 # Create or read your DataFrame
-strem_nm = 'STREM_ABC'
-df = pd.read_csv(f'/mnt/shared/Toh/processing/{strem_nm}.csv')
+data = {
+    "prcs_nm": ["ABC_1", "ABC_2", "ABC_3", "ABC_4","ABC_5","ABC_6"],
+    "strem_nm": ["STREM_ABC"] * 6,
+    "prir": [1, 2, 2, 3, 3, 4]
+}
+df = pd.DataFrame(data)
+
 
 default_args = {
     'owner': 'airflow',
